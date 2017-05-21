3eb4b90fdc0edef5fb4cb51c6f51366971db6c4b
Apavayan Sinha <info@apavayan.com>
2017-05-21 14:38:01 +0530

    GPU update
    Change-Id: I09875c732ba0da980b781c1361371d9ee48b64a4

--- drivers/gpu/arm/t72x/r7p0/mali_kbase_mem_profile_debugfs.c
+++ drivers/gpu/arm/t72x/r7p0/mali_kbase_mem_profile_debugfs.c
@@ -48,6 +48,10 @@ static int kbasep_mem_profile_seq_show(struct seq_file *sfile, void *data)
 	struct kbase_context *kctx = sfile->private;
 
 	KBASE_DEBUG_ASSERT(kctx != NULL);
+	
+	/* MALI_SEC_INTEGRATION - Destroyed context */
+	if (kctx == NULL)
+		return 0;
 
 	/* MALI_SEC_INTEGRATION */
 	{
